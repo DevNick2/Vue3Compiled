84682edf7c397729eae1676f84a0fde22cf391bb
Jean Nícolas <nickemanta@gmail.com>
2020-10-14 21:23:06 -0300

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- Fragment Example -->
+  <composition-api/>
+  <extraction-and-reuse/>
+  <style-variables/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CompositionApi from './components/CompositionApi'
+import ExtractionAndReuse from './components/ExtractionAndReuse'
+import StyleVariables from './components/StyleVariables'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    CompositionApi,
+    ExtractionAndReuse,
+    StyleVariables
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
